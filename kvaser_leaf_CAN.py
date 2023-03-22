36e578ce2347f4b0629aaa0fc015c52394382ff5
MateKristof <mate.kry@gmail.com>
2023-03-22 14:26:19 +0100

    update

--- kvaser_leaf_CAN.py
+++ kvaser_leaf_CAN.py
@@ -3,18 +3,24 @@ import can
 import binascii
 import struct
 import codecs
-
+from time import sleep
 
 
 # create a bus instance
 # many other interfaces are supported as well (see documentation)
-bus = can.Bus(interface='socketcan',
-              channel='can0',
+bus = can.Bus(interface='kvaser',
+              channel=0,
               receive_own_messages=True)
 
 # send a message
-message = can.Message(arbitration_id=123, is_extended_id=True, data=[0x11, 0x22, 0x33])
+message = can.Message(arbitration_id=0x708, is_extended_id=True, data=[0x20, 0x01])
 bus.send(message, timeout=0.2)
+
+sleep(1)
+message = can.Message(arbitration_id=0x708, is_extended_id=True, data=[0x30, 0x01])
+bus.send(message, timeout=0.2)
+
+
 
 # iterate over received messages
 for msg in bus:
