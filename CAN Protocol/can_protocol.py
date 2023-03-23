e5e8f92a91b272d27cd17430aa6fa9ab8aa26c0e
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2023-03-23 18:16:46 +0100

    :heavy_plus_sign:  Config

--- CAN Protocol/can_protocol.py
+++ CAN Protocol/can_protocol.py
@@ -20,46 +20,91 @@ class Can:
         self.bus.send(message, timeout=0.2)
 
     def start_motors(self):
+
+        self.cmd_drive()
+
+        message = can.Message(arbitration_id=0x58D, is_extended_id=False, data=[0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+        message = can.Message(arbitration_id=0x595, is_extended_id=False, data=[0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+        message = can.Message(arbitration_id=0x59D, is_extended_id=False, data=[0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+        message = can.Message(arbitration_id=0x5A5, is_extended_id=False, data=[0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+        message = can.Message(arbitration_id=0x58D, is_extended_id=False,data=[0x02, 0x00, 0x00, 0x00, 0x44, 0x7A, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+        message = can.Message(arbitration_id=0x595, is_extended_id=False, data=[0x02, 0x00, 0x00, 0x00, 0x44, 0x7A, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+        message = can.Message(arbitration_id=0x59D, is_extended_id=False, data=[0x02, 0x00, 0x00, 0x00, 0x44, 0x7A, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+        message = can.Message(arbitration_id=0x5A5, is_extended_id=False, data=[0x02, 0x00, 0x00, 0x00, 0x44, 0x7A, 0x00, 0x00])
+        self.bus.send(message, timeout=0.2)
+        sleep(1)
+
+    def cmd_drive(self):
         # CMD mode drive
         message = can.Message(arbitration_id=0x588, is_extended_id=False, data=[0x10, 0x00, 0x12, 0x00])
         self.bus.send(message, timeout=0.2)
-        # CMD drive state started
-        message = can.Message(arbitration_id=0x588, is_extended_id=False, data=[0x50, 0x00, 0x01, 0x00])
-        self.bus.send(message, timeout=0.2)
+
         # CMD mode drive
         message = can.Message(arbitration_id=0x590, is_extended_id=False, data=[0x10, 0x00, 0x12, 0x00])
         self.bus.send(message, timeout=0.2)
-        # CMD drive state started
-        message = can.Message(arbitration_id=0x590, is_extended_id=False, data=[0x50, 0x00, 0x01, 0x00])
-        self.bus.send(message, timeout=0.2)
+
         # CMD mode drive
         message = can.Message(arbitration_id=0x598, is_extended_id=False, data=[0x10, 0x00, 0x12, 0x00])
         self.bus.send(message, timeout=0.2)
-        # CMD drive state started
-        message = can.Message(arbitration_id=0x598, is_extended_id=False, data=[0x50, 0x00, 0x01, 0x00])
-        self.bus.send(message, timeout=0.2)
+
         # CMD mode drive
         message = can.Message(arbitration_id=0x5A0, is_extended_id=False, data=[0x10, 0x00, 0x12, 0x00])
         self.bus.send(message, timeout=0.2)
+
+        # CMD drive state started
+        message = can.Message(arbitration_id=0x588, is_extended_id=False, data=[0x50, 0x00, 0x01, 0x00])
+        self.bus.send(message, timeout=0.2)
+
+        # CMD drive state started
+        message = can.Message(arbitration_id=0x590, is_extended_id=False, data=[0x50, 0x00, 0x01, 0x00])
+        self.bus.send(message, timeout=0.2)
+
+        # CMD drive state started
+        message = can.Message(arbitration_id=0x598, is_extended_id=False, data=[0x50, 0x00, 0x01, 0x00])
+        self.bus.send(message, timeout=0.2)
+
         # CMD drive state started
         message = can.Message(arbitration_id=0x5A0, is_extended_id=False, data=[0x50, 0x00, 0x01, 0x00])
         self.bus.send(message, timeout=0.2)
         sleep(1)
 
+
+
     def drive(self):
         message = can.Message(arbitration_id=0x58A, is_extended_id=False,
-                              data=[0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00])
+                              data=[0x42, 0xc8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
         self.bus.send(message, timeout=0.2)
 
         message = can.Message(arbitration_id=0x592, is_extended_id=False,
-                              data=[0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00])
+                              data=[0x42, 0xc8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
         self.bus.send(message, timeout=0.2)
 
         message = can.Message(arbitration_id=0x59A, is_extended_id=False,
-                              data=[0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00])
+                              data=[0x42, 0xc8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
         self.bus.send(message, timeout=0.2)
 
         message = can.Message(arbitration_id=0x5A2, is_extended_id=False,
-                              data=[0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00])
+                              data=[0x42, 0xc8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
         self.bus.send(message, timeout=0.2)
 
