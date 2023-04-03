83f4f2f2c4cd61a4a987b516ca6404fdf23b6fb2
BPelenczei <balint@pelenczei.hu>
2023-04-03 17:37:18 +0200

    ADD main control multiprocessing

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from jarmuiranyitas_2.can.run import CAN
+from multiprocessing import Process
+
+from jarmuiranyitas_2.can.can import CAN
+from jarmuiranyitas_2.udp.IPv6_udp_receiver import UDPReceiver
 
 
 def main():
-    can_comm = CAN()
+    can = CAN()
+    udp = UDPReceiver()
 
-    can_comm.start_communication()
+    can_comm = Process(target=can.start_communication)
+    udp_listener = Process(target=udp.receive)
+
+    can_comm.start()
+    udp_listener.start()
+
+    while True:
+        can.set_ref_vals(udp.last_data)
 
 
 if __name__ == "__main__":
