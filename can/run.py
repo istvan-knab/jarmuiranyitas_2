859caa9fb2a39cfa6cbb39b70796e69614508c3a
BPelenczei <balint@pelenczei.hu>
2023-04-03 15:52:49 +0200

    CHG can communication script structure

--- can/run.py
+++ can/run.py
@@ -1,38 +1,37 @@
-from multiprocessing import Process
-
 from jarmuiranyitas_2.can.config import Config
 from jarmuiranyitas_2.can.network import CANNetwork
 from jarmuiranyitas_2.can.internal_states import InternalStates
 from jarmuiranyitas_2.can.state_handler import StateHandler
 
 
-def main():
-    config = Config()
-    network = CANNetwork(interface=config.get_interface(), channel=config.get_channel(), bitrate=config.get_bitrate(),
-                         receive_own_messages=config.get_receive_own_messages())
-    state_handler = StateHandler(can_network=network, init_state=InternalStates.START1)
+class CAN:
 
-    current_state = state_handler.get_current_state()
+    def __init__(self):
+        self.config = Config()
+        self.network = CANNetwork(interface=self.config.get_interface(),
+                                  channel=self.config.get_channel(),
+                                  bitrate=self.config.get_bitrate(),
+                                  receive_own_messages=self.config.get_receive_own_messages())
+        self.state_handler = StateHandler(can_network=self.network, init_state=InternalStates.START1)
 
-    while True:
-        if current_state == InternalStates.START1:
-            current_state = state_handler.handle_start1()
+        self.current_state = self.state_handler.get_current_state()
 
-        elif current_state == InternalStates.START2:
-            current_state = state_handler.handle_start2()
+    def start_communication(self):
+        while True:
+            if self.current_state == InternalStates.START1:
+                self.current_state = self.state_handler.handle_start1()
 
-        elif current_state == InternalStates.START3:
-            current_state = state_handler.handle_start3()
+            elif self.current_state == InternalStates.START2:
+                self.current_state = self.state_handler.handle_start2()
 
-        elif current_state == InternalStates.IDLE:
-            current_state = state_handler.handle_idle()
+            elif self.current_state == InternalStates.START3:
+                self.current_state = self.state_handler.handle_start3()
 
-        elif current_state == InternalStates.DRIVE:
-            current_state = state_handler.handle_drive()
+            elif self.current_state == InternalStates.IDLE:
+                self.current_state = self.state_handler.handle_idle()
 
-        elif current_state == InternalStates.ERR:
-            current_state = state_handler.handle_err()
+            elif self.current_state == InternalStates.DRIVE:
+                self.current_state = self.state_handler.handle_drive()
 
-
-if __name__ == '__main__':
-    main()
+            elif self.current_state == InternalStates.ERR:
+                self.current_state = self.state_handler.handle_err()
