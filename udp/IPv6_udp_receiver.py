b7fa3f187e270874d26f49175c72e9dc1fb8b9af
BPelenczei <balint@pelenczei.hu>
2023-04-06 11:05:14 +0200

    FIX udp receiver ip address

--- udp/IPv6_udp_receiver.py
+++ udp/IPv6_udp_receiver.py
@@ -5,9 +5,9 @@ from time import sleep
 
 class UDPReceiver:
 
-    def __init__(self, receive_from: str = '::', port: int = 5005):
+    def __init__(self, receive_from: str = 'Orin', port: int = 5005):
         self.udp_port = port
-        self.udp_ip = receive_from
+        self.udp_ip = socket.getaddrinfo(receive_from, port, family=socket.AF_INET6, proto=socket.IPPROTO_UDP)[0][4][0]
         self.sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
         self.sock.bind((self.udp_ip, self.udp_port))
 
