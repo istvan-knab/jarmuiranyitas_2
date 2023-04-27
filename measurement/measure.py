260a2268aed56d5d7d13ac348f231dcd47976ddb
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2023-04-27 14:39:53 +0200

    change spi to can
    Imu will log the data through can , we won't use spi

--- measurement/measure.py
+++ measurement/measure.py
@@ -1,9 +1,15 @@
+import can
+from can import Message
+
 class Measure:
     """
-    This class is the abstraction of measure yaw rate to get function between that and
-    input signal . With measuring that data we will find correlation between steering angle
+    This class is the abstraction of measure yaw rate to get function between that and the
+    input signal . With measuring that data our goal is to find correlation between steering angle
     and torque distribution by the certain points , and if we have the necessary amount of data,
     we will fit a curve
+
+    The reference yaw rate will be measured by a heat compensated imu ( Inertial Measurement Unit), which
+    will log the actual yaw rate by CAN-signals, like the other connections in the car
     """
     def __init__(self):
         self.velocity = 0
