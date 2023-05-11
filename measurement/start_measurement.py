d7d356b0bc84d58bfffed6c2d7ba0539ae6b1030
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2023-05-11 09:08:54 +0200

    updating dict file, only once at 100 measurement

--- measurement/start_measurement.py
+++ measurement/start_measurement.py
@@ -1,18 +1,25 @@
+from time import sleep
+
 from jarmuiranyitas_2.measurement.measure_distribution import MeasureTorque
 from jarmuiranyitas_2.measurement.measure_steering_angle import MeasureAngle
 class StartMeasurement:
     def __init__(self, mode:str, frequency:int) -> None:
-
+        self.frequency = frequency
         if mode == "distribution":
-            measure = MeasureTorque()
+            self.measure = MeasureTorque()
         elif mode == "steering":
-            measure = MeasureAngle()
+            self.measure = MeasureAngle()
         else:
             raise Exception("Invalid measurement mode")
     def active(self):
-
+        counter = 0
         while True:
-            pass
+            counter += 1
+            sleep(1 / self.frequency)
+            self.measure.update_state_dict()
+            if counter == 100:
+                self.measure.write_file()
+                counter = 0
 
 
 start= StartMeasurement("distribution", 1200)
